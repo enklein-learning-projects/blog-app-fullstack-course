b608aaa235a8127aef43b600ac713422a5ad8a45
pandabeards <zach@pandabeards.io>
2020-03-08 19:45:28 -0400

    added missing }

--- models/User.js
+++ models/User.js
@@ -5,8 +5,8 @@ let User = function(data) {
     this.errors = []
 }
 
-User.prototype.validate = function () {
-    if (this.data.username == "") {this.errors.push("You must provide a username.")
+User.prototype.validate = function() {
+    if (this.data.username == "") {this.errors.push("You must provide a username.")}
     if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) {this.errors.push("Username can only contain letters and numbers.")}
     if (!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address.")}
     if (this.data.password == "") {this.errors.push("You must provide a password.")}
@@ -14,7 +14,6 @@ User.prototype.validate = function () {
     if (this.data.password.length > 100) {this.errors.push("Password cannot exceed 100 characters.")}
     if (this.data.username.length > 0 && this.data.username.length < 3) {this.errors.push("Password must be at least 3 characters.")}
     if (this.data.username.length > 30) {this.errors.push("Username cannot exceed 30 characters.")}
-}
 }
 
 User.prototype.register = function() {
